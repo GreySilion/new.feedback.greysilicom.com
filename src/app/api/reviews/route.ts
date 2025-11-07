ff2a4d2fc9f0bce941a155c8838329fe9d7cf55a
Brian Peace Teka <peaceteka2@gmail.com>
2025-11-07 15:10:34 +0300

    finalized

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 import { NextResponse } from 'next/server';
 import { RowDataPacket } from 'mysql2';
 import { getServerSession } from 'next-auth';
